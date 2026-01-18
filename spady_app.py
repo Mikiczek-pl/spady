6c0a61b6e10929ca486f1b6ee19c84a0fb0c94e8
Mikiczek-pl <czekalskimikolaj@gmail.com>
2026-01-18 23:38:31 +0100

    Update spady_app.py

--- spady_app.py
+++ spady_app.py
@@ -14,7 +14,7 @@ STRETCH_MM = 5.0
 BLEED_MM = STRETCH_MM - STRIP_MM  # 3 mm
 DPI = 300
 
-LOGO_PATH = "assets/CR.png"  # <- Twoje logo
+LOGO_PATH = "assets/logo CR.png"  # <- Twoje logo
 
 
 # =========================
@@ -337,3 +337,4 @@ st.markdown("""
   if (el) el.scrollIntoView({behavior: "smooth", block: "start"});
 </script>
 """, unsafe_allow_html=True)
+
